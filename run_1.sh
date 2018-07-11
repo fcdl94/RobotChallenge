a0987cef03a9b34468a49da906ffba9f2f11500d
Fabio Cermelli <f.cermelli94@gmail.com>
2018-07-11 09:58:09 +0200

    Cambiato script per run. Nuovi parametri 1e-3, bs 64, step 15, epoche 30 Nuovi parametri 1e-4, bs 64, step 40, epoche 60

--- run_1.sh
+++ run_1.sh
@@ -4,7 +4,5 @@ source activate fabio_robot
 export CUDA_VISIBLE_DEVICES=0
 
 #split 1
-python main.py  --prefix ROD_models/S1LR3BS64 --folder /home/fabio/robot_challenge/rod/split1 --epochs 30 --visdom ROD1_LR3BS64 --lr 0.001
-python main.py  --prefix ROD_models/S1LR4BS64 --folder /home/fabio/robot_challenge/rod/split1 --epochs 30 --visdom ROD1_LR4BS64 --lr 0.0001
-python main.py  --prefix ROD_models/S1LR3BS128 --folder /home/fabio/robot_challenge/rod/split1 --epochs 30 --visdom ROD1_LR3BS128 --lr 0.001 --bs 128
-python main.py  --prefix ROD_models/S1LR4BS128 --folder /home/fabio/robot_challenge/rod/split1 --epochs 30 --visdom ROD1_LR4BS128 --lr 0.0001 --bs 128
+python main.py  --prefix ROD_models/S1LR3BS64S15E30 --folder /home/fabio/robot_challenge/rod/split1 --step 15 --epochs 30 --visdom ROD1_LR3BS64S15E30 --lr 0.001
+python main.py  --prefix ROD_models/S1LR4BS64S40E60 --folder /home/fabio/robot_challenge/rod/split1 --step 40 --epochs 60 --visdom ROD1_LR4BS64S40E60 --lr 0.0001
