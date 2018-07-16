6bba1c59b3a1fcce9608d709d689b14599b299a0
fcdl94 <f.cermelli94@gmail.com>
2018-07-16 10:43:25 +0200

    Scripts scripts scripts

--- run_x3.sh
+++ run_x3.sh
@@ -3,17 +3,15 @@ source activate fabio_robot
 
 export CUDA_VISIBLE_DEVICES=2
 
-#python main.py  --prefix RODx_models/S1LR23BS64t1 --folder /home/fabio/robot_challenge/rod/split1 --visdom RODx_S1LR23BS32T1 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32 
+#python main.py  --prefix RODx_models/S1LR23BS64t1 --folder /home/fabio/robot_challenge/rod/split1 --visdom RODx_S1LR23BS32D4T1 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
+#python main.py  --prefix RODx_models/S1LR23BS64t2 --folder /home/fabio/robot_challenge/rod/split1 --visdom RODx_S1LR23BS32D4T2 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
+#python main.py  --prefix RODx_models/S1LR23BS64t3 --folder /home/fabio/robot_challenge/rod/split1 --visdom RODx_S1LR23BS32D4T3 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
 
-#python main.py  --prefix RODx_models/S1LR23BS64t2 --folder /home/fabio/robot_challenge/rod/split1 --visdom RODx_S1LR23BS32T2 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32 
-
-#python main.py  --prefix RODx_models/S1LR23BS64t3 --folder /home/fabio/robot_challenge/rod/split1 --visdom RODx_S1LR23BS32T3 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32 
-
-#python main.py  --prefix RODx_models/S2LR23BS64t1 --folder /home/fabio/robot_challenge/rod/split2 --visdom RODx_S2LR23BS32T1 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32
-#python main.py  --prefix RODx_models/S2LR23BS64t2 --folder /home/fabio/robot_challenge/rod/split2 --visdom RODx_S2LR23BS32T2 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32
-#python main.py  --prefix RODx_models/S2LR23BS64t3 --folder /home/fabio/robot_challenge/rod/split2 --visdom RODx_S2LR23BS32T3 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32
+#python main.py  --prefix RODx_models/S2LR23BS64t1 --folder /home/fabio/robot_challenge/rod/split2 --visdom RODx_S2LR23BS32D4T1 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
+#python main.py  --prefix RODx_models/S2LR23BS64t2 --folder /home/fabio/robot_challenge/rod/split2 --visdom RODx_S2LR23BS32D4T2 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
+#python main.py  --prefix RODx_models/S2LR23BS64t3 --folder /home/fabio/robot_challenge/rod/split2 --visdom RODx_S2LR23BS32D4T3 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
 
 
-python main.py  --prefix RODx_models/S3LR23BS64t1 --folder /home/fabio/robot_challenge/rod/split3 --visdom RODx_S3LR23BS32T1 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32
-python main.py  --prefix RODx_models/S3LR23BS64t2 --folder /home/fabio/robot_challenge/rod/split3 --visdom RODx_S3LR23BS32T2 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32
-python main.py  --prefix RODx_models/S3LR23BS64t3 --folder /home/fabio/robot_challenge/rod/split3 --visdom RODx_S3LR23BS32T3 --step 20 --epochs 30 --lr 0.005 --decay 0.00005 --bs 32
+python main.py  --prefix RODx_models/S3LR23BS64t1 --folder /home/fabio/robot_challenge/rod/split3 --visdom RODx_S3LR23BS32D4T1 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
+python main.py  --prefix RODx_models/S3LR23BS64t2 --folder /home/fabio/robot_challenge/rod/split3 --visdom RODx_S3LR23BS32D4T2 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
+python main.py  --prefix RODx_models/S3LR23BS64t3 --folder /home/fabio/robot_challenge/rod/split3 --visdom RODx_S3LR23BS32D4T3 --step 20 --epochs 30 --lr 0.005 --decay 0.0005 --bs 32
