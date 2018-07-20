bb0b6d96a361c852ecb09aa377634d3d54030ac9
Fabio Cermelli <f.cermelli94@gmail.com>
2018-07-20 15:57:51 +0200

    Fix fix fix

--- DIAL/utils.py
+++ DIAL/utils.py
@@ -13,11 +13,9 @@ def check_equals(dict1, dict2):
 
 def check_equals_bn(dict1, dict2):
     flag = True
-    for val in dict2.values():
-        val.cpu()
     for key in dict2.keys():
         if "running" not in key:
-            if not t.equal(dict1[key], dict2[key]):
+            if not t.equal(dict1[key].cpu(), dict2[key].cpu()):
                 flag = False
                 print(key)
     return flag
